7ff1cfd9dddf2d239183502063567e20fe2a6a5f
Crystal Studio <notwirachai@gmail.com>
2023-04-24 10:22:43 +0700

    new city

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,21 @@
 module.exports = [
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  ,
+  "strapi::security",
+  // {
+  //   name: "strapi::security",
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         "connect-src": ["'self'", "https:"],
+  //         "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+  //         "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
+  // ,
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,5 +16,22 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ["plugin::users-permissions.user"],
+
+      async afterCreate(event) {
+        const { result, params } = event;
+        console.log("result:>>", result);
+        await strapi.entityService.create("api::city.city", {
+          data: {
+            X: Math.floor(Math.random() * 1201),
+            Y: Math.floor(Math.random() * 1201),
+            user: result.id,
+            publishedAt: new Date(),
+          },
+        });
+      },
+    });
+  },
 };
