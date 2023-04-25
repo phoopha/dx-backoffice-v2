545541756424ba1d0fde13bfd721ca9e61d69e19
Niphawan Phoopha <niphawanphoopha@192.168.1.33>
2023-04-25 10:15:45 +0700

    fix(city): me

--- src/api/city/controllers/city.js
+++ src/api/city/controllers/city.js
@@ -13,16 +13,13 @@ module.exports = createCoreController("api::city.city", ({ strapi }) => ({
 
   async myCity(ctx) {
     console.log("ctx params :> ", ctx);
-    await strapi.db
-      .query("api::city.city")
-      .myCity
-      //   {
-      //   where: {
-      //     title: {
-      //       $startsWith: "v3",
-      //     },
-      //   },
-      // }
-      ();
+    await strapi.db.query("api::city.city").myCity();
+    //   {
+    //   where: {
+    //     title: {
+    //       $startsWith: "v3",
+    //     },
+    //   },
+    // }
   },
 }));
