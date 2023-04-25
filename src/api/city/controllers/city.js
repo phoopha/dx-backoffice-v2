f17f6d8ddc7af7ee7eaf6a8e80a539f266b96886
Niphawan Phoopha <niphawanphoopha@192.168.1.33>
2023-04-25 11:58:11 +0700

    fix(city): delete comment

--- src/api/city/controllers/city.js
+++ src/api/city/controllers/city.js
@@ -8,10 +8,6 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::city.city", ({ strapi }) => ({
   async deleteMany(ctx) {
-    // console.log(
-    //   "STRAPI:>>",
-    //   strapi.db.query("api::city.city", { where: { id: 1031 } })
-    // );
     await strapi.db.query("api::city.city").deleteMany();
   },
 }));
